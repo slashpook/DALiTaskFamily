6f72dded02546cda4e6c6bd02e66fa84a09747cc
DELES <deles.damien@gmail.com>
2014-04-27 16:13:32 +0200

    Création des méthodes de suppression des objets

--- DALiTaskFamily/DALiTaskFamily/DDDatabaseAccess.h
+++ DALiTaskFamily/DALiTaskFamily/DDDatabaseAccess.h
@@ -28,4 +28,46 @@
 //Annulation de l'action
 - (void)rollback;
 
+
+#pragma mark - CRUD Achievement
+
+//On supprime l'achievement donné
+-(void)deleteAchievement:(Achievement *)achievement;
+
+
+#pragma mark - CRUD CategoryTask
+
+//On supprime la categoryTask donnée
+-(void)deleteCategoryTask:(CategoryTask *)categoryTask;
+
+
+#pragma mark - CRUD CategoryTrophy
+
+//On supprime le categoryTrophy donné
+-(void)deleteCategoryTrophy:(CategoryTrophy *)categoryTrophy;
+
+
+#pragma mark - CRUD Event
+
+//On supprime l'event donné
+-(void)deleteEvent:(Event *)event;
+
+
+#pragma mark - CRUD Player
+
+//On supprime le player donné
+-(void)deletePlayer:(Player *)player;
+
+
+#pragma mark - CRUD Task
+
+//On supprime la task donnée
+-(void)deleteTask:(Task *)task;
+
+
+#pragma mark - CRUD Trophy
+
+//On supprime le trophy donné
+-(void)deleteTrophy:(Trophy *)trophy;
+
 @end
--- DALiTaskFamily/DALiTaskFamily/DDDatabaseAccess.m
+++ DALiTaskFamily/DALiTaskFamily/DDDatabaseAccess.m
@@ -41,8 +41,9 @@
 #pragma mark - Base Methods
 
 //On sauvegarde les données
-- (BOOL)saveContext:(NSError *)error
+- (BOOL)saveContext
 {
+    NSError *error = nil;
     return [self.dataBaseManager saveContext:error];
 }
 
@@ -50,6 +51,76 @@
 - (void)rollback
 {
     [self.dataBaseManager.managedObjectContext rollback];
+}
+
+
+#pragma mark - CRUD Achievement
+
+//On supprime l'achievement donné
+-(void)deleteAchievement:(Achievement *)achievement
+{
+    [self.dataBaseManager.managedObjectContext deleteObject:achievement];
+    [self saveContext];
+}
+
+
+#pragma mark - CRUD CategoryTask
+
+//On supprime la categoryTask donnée
+-(void)deleteCategoryTask:(CategoryTask *)categoryTask
+{
+    [self.dataBaseManager.managedObjectContext deleteObject:categoryTask];
+    [self saveContext];
+}
+
+
+#pragma mark - CRUD CategoryTrophy
+
+//On supprime le categoryTrophy donné
+-(void)deleteCategoryTrophy:(CategoryTrophy *)categoryTrophy
+{
+    [self.dataBaseManager.managedObjectContext deleteObject:categoryTrophy];
+    [self saveContext];
+}
+
+
+#pragma mark - CRUD Event
+
+//On supprime l'event donné
+-(void)deleteEvent:(Event *)event
+{
+    [self.dataBaseManager.managedObjectContext deleteObject:event];
+    [self saveContext];
+}
+
+
+#pragma mark - CRUD Player
+
+//On supprime le player donné
+-(void)deletePlayer:(Player *)player
+{
+    [self.dataBaseManager.managedObjectContext deleteObject:player];
+    [self saveContext];
+}
+
+
+#pragma mark - CRUD Task
+
+//On supprime la task donnée
+-(void)deleteTask:(Task *)task
+{
+    [self.dataBaseManager.managedObjectContext deleteObject:task];
+    [self saveContext];
+}
+
+
+#pragma mark - CRUD Trophy
+
+//On supprime le trophy donné
+-(void)deleteTrophy:(Trophy *)trophy
+{
+    [self.dataBaseManager.managedObjectContext deleteObject:trophy];
+    [self saveContext];
 }
 
 @end
