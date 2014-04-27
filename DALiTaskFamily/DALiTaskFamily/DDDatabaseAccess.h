f22fb6fb91e3b49f54d2d9ea2d6b46e09c24100c
DELES <deles.damien@gmail.com>
2014-04-27 15:54:17 +0200

    Création du fichier DatabaseAccess avec mise en place des fonctions de base

---
+++ DALiTaskFamily/DALiTaskFamily/DDDatabaseAccess.h
@@ -0,0 +1,31 @@
+//
+//  DDDatabaseAccess.h
+//  DALiTaskFamily
+//
+//  Created by Damien DELES on 27/04/2014.
+//  Copyright (c) 2014 Damien DELES. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "DDDatabaseManager.h"
+
+@interface DDDatabaseAccess : NSObject
+
+#pragma mark - Variables
+
+//Instance de la base de donnée
+@property (strong, nonatomic) DDDatabaseManager *dataBaseManager;
+
+
+#pragma mark - Base Methods / save methods
+
+//Initialisation du singleton
++ (DDDatabaseAccess *)instance;
+
+//Sauvegare
+- (BOOL)saveContext:(NSError *)error;
+
+//Annulation de l'action
+- (void)rollback;
+
+@end
---
+++ DALiTaskFamily/DALiTaskFamily/DDDatabaseAccess.m
@@ -0,0 +1,55 @@
+//
+//  DDDatabaseAccess.m
+//  DALiTaskFamily
+//
+//  Created by Damien DELES on 27/04/2014.
+//  Copyright (c) 2014 Damien DELES. All rights reserved.
+//
+
+#import "DDDatabaseAccess.h"
+
+@implementation DDDatabaseAccess
+
+
+#pragma mark - Init
+
+//Instance du singleton
++ (DDDatabaseAccess *)instance
+{
+	static DDDatabaseAccess *instance;
+	
+	@synchronized(self) {
+		if(!instance) {
+			instance = [[DDDatabaseAccess alloc] init];
+		}
+	}
+	
+	return instance;
+}
+
+-(id) init
+{
+    self = [super init];
+    if (self != nil)
+    {
+        self.dataBaseManager = [[DDDatabaseManager alloc] init];
+    }
+    return self;
+}
+
+
+#pragma mark - Base Methods
+
+//On sauvegarde les données
+- (BOOL)saveContext:(NSError *)error
+{
+    return [self.dataBaseManager saveContext:error];
+}
+
+//On annule les modifications
+- (void)rollback
+{
+    [self.dataBaseManager.managedObjectContext rollback];
+}
+
+@end
